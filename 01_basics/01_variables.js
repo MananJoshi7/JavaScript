ca06aadd338afe5f707cf1bc4948ffa84bc6b96a
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-05 13:25:06 +0000

    break

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 789
+let accountEmail = "man@gmail.com";
+var accountPassword = "1234";
+accountCity = "Rajkot";
+console.table([accountId, accountEmail, accountPassword, accountCity]);
