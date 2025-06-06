c372b46dcf85ec89f7fbaa9b9fe4b28e05e66434
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-06 12:12:24 +0000

    conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "59abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+
+let isLoggedIn = "man";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 9;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
