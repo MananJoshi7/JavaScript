1d0fd064f40b3ee2e224442c27a1ff9648c515e7
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-07 08:05:08 +0000

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive datatypes
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const name = "Man";
+// const score = 100;
+// const isLoggedIn = false;
+// const outsideTemp = null;
+// let username;
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+// console.log(id === a);
+
+
+// Non Primitive datatypes (Reference datatypes)
+// Array, Objects, Functions
+const heros = ["shakitman","naagraj","doga"];
+let myObj = {
+    name: "Manan",
+    age: 21
+}
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+// Type of value                                Result
+// Undefined                                  "undefined"
+// Null                                       "object"
+// Boolean                                    "boolean"  
+// Number                                     "number"
+// String                                     "string"
+// Object(native and does not                 "object"
+//     implement [[Call]])
+// Object(native or host and                  "function"
+//     does implement [[Call]])
