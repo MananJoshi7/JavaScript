5eaeca4f75e9b5758d6af16432c6bab166150724
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-10 09:18:03 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -25,7 +25,47 @@ console.log(words.charAt(1)); // "B"
 console.log(words.charAt(2)); // "C"
 
 // String.prototype.charCodeAt()
+const character = "XYZ";
+console.log(character.charCodeAt('X'));
 
+// String.prototype.codePointAt()
+const code = "PQR";
+console.log(code.codePointAt('P'));
+
+// String.prototype.concat()
+const str = "Hello";
+console.log(str.concat(" World!"));
+
+// String.prototype.endsWith()
+const str1 = "Hello World";
+console.log(str1.endsWith("World")); // true
+console.log(str1.endsWith("Hello")); // false
+console.log(str1.endsWith('lo', 5)); // true
+
+// String.prototype.fixed()
+const str2 = "Code Sample";
+console.log(str2.fixed()); // <tt>Code Sample</tt>
+
+//String.prototype.fontcolor()
+const str3 = "Warning message";
+console.log(str3.fontcolor("red")); // <font color="red">Warning message</font>
+
+// String.prototype.fontsize()
+const str4 = "Important Notice";
+console.log(str4.fontsize(5)); // <font size="5">Important Notice</font>
+
+// String.prototype.includes()
+const str5 = "JavaScript is Awesome!";
+console.log(str5.includes('Script')); // true
+console.log(str5.includes("script")); // false (case-sensitive)
+console.log(str5.includes('is', 10)); // false 
+
+// String.prototype.indexOf()
+const str6 = "Hello world";
+console.log(str6.indexOf('world')); // 6
+console.log(str6.indexOf('World')); // -1 (case-senstivie)
+console.log(str6.indexOf('o')); // 4 (first 'o')
+console.log(str6.indexOf('o',5)) // 7 (search starts at position 5)
 
 
 
@@ -49,6 +89,7 @@ console.log(newString);
 const anotherString = gameName.slice(-5,4);
 console.log(anotherString);
 // under a slice we can give the neagtive value too.
+
 
 const newStringOne = "   Manan   ";
 console.log(newStringOne);
