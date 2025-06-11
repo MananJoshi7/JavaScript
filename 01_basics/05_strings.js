1444e8937fd0ee147a1557d478cc626cac52ea1b
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-11 04:11:24 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -67,6 +67,73 @@ console.log(str6.indexOf('World')); // -1 (case-senstivie)
 console.log(str6.indexOf('o')); // 4 (first 'o')
 console.log(str6.indexOf('o',5)) // 7 (search starts at position 5)
 
+// String.prototype.link()
+const str7 = "Click Here";
+console.log(str7.link("https.example.com")); // <a href="https.example.com">Click Here</a>
+
+// String.prototype.localeCompare()
+const str8 = "a"
+const str9 = "b";
+const str10 = "a";
+console.log(str8.localeCompare("b")); // -1 (a comes before b)
+console.log(str9.localeCompare("a")); // 1 (b comes after a)
+console.log(str10.localeCompare("a")); // 0 (equal)
+
+// String.prototype.match()
+const str11 = "The rain in spain";
+console.log(str11.match('ain')); // [ 'ain', index: 5, input: 'The rain in spain', groups: undefined ]
+
+// String.prototype.matchAll()
+const str12 = 'test1test2';
+const regex = /t(e)(st(\d?))/g;
+const matches = str12.matchAll(regex);
+for (const match of matches) {
+  console.log(match);
+}
+
+// String.prototype.normalize()
+const str13 = "é";          // "\u00E9" (single character)
+const str14 = "é";          // "\u0065\u0301" (e + combining ´)
+console.log(str13 === str14); // false (different representations)
+console.log(str13.normalize() === str14.normalize()); // true (now equal)
+
+// String.prototype.padEnd()
+const str15 = "Hello";
+console.log(str15.padEnd(10));      // "Hello     " (5 spaces added)
+console.log(str15.padEnd(10, " ")); // Same as above
+
+// String.prototype.padStart()
+const str16 = "Hi";
+console.log(str16.padStart(5));
+
+// String.prototype.repeat()
+const str17 = "Hi";
+const str18 = "Na";
+console.log(str17.repeat(3)); // HiHiHi
+console.log(str18.repeat(2)+" Batman!"); // NaNa Batman!
+
+// String prototype.replaca()
+const str19 = "Hello World";
+console.log(str19.replace("World", "Universe")); //Hello Universe
+
+// String prototype.replaceAll()
+const str20 = "Apples are sweet, apples are juicy";
+console.log(str20.replaceAll("apples, oranges")); // Error! (Must use regex for case-insensitive)
+console.log(str20.replaceAll(/apples/gi,"Oranges")); // "Oranges are sweet, Oranges are juicy."
+
+// String prototype.search()
+const str21 = "Hello World";
+console.log(str21.search("World")); // 6 (starts at index 6)
+console.log(str21.search("world"));  // -1 (case-sensitive)
+
+// String prototype.slice()
+const str22 = "Hello World";
+console.log(str22.slice(6)); // "World" (from index 6 to end)
+console.log(str22.slice(0,5)); // "Hello" (index 0 to 4)
+
+
+
+
 
 
 const name = "Manan";
