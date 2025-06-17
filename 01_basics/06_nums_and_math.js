7eba147de3ed22a4b2548c92094d227fbd565a92
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-17 02:51:44 +0000

    num

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -19,3 +19,14 @@ console.log(balance.toFixed(2));
 
 const hundreds = 1000000;
 console.log(hundreds.toLocaleString('en-IN'));
+
+// toFixed() Method
+const number = 123.4567;
+console.log(number.toFixed(2)); // Returns "123.46" (rounded to 2 decimals)
+
+// toLocaleString() Method
+const number1 = 1234567.894;
+console.log(number1.toLocaleString('en-IN')); // Returns "12,34,567.894" (INDIAN format)
+console.log(number1.toLocaleString('en-US')); // Returns "1,234,567.894" (US format)
+console.log(number1.toLocaleString('en-DE')); // Returns "1.234.567,894" (GERMAN format)
+
