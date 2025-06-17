30abb4294c49372a1557b6ee1cca22c20a169065
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-17 07:32:06 +0000

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,8 @@
+const myArr = [1,2,3,4,5];
+myArr.push(6); // Adds element in the array
+myArr.pop(); // Removes the last element from the array
+myArr.unshift(9); // Adds element at the starting of the array
+myArr.shift(); // Removes the first element from the array
+console.log( myArr.includes(9)); // false
+console.log(myArr.indexOf(4)); // 3
+// console.log(myArr); 
