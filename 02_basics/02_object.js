f7aa9ddbcdcf4b7fee0d95eecedaac1190ba2746
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-19 09:22:27 +0000

    objects

--- 02_basics/02_object.js
+++ 02_basics/02_object.js
@@ -3,6 +3,7 @@
 
 // object literals
 const mySym = Symbol("key1");
+const mySym1 = Symbol("user123");
 const JsUser = {
     name:"Manan",
     "full name": "Manan Joshi",
@@ -12,10 +13,26 @@ const JsUser = {
     email:"manan@gmail.com",
     isLoggedIn: false,
 }
+
+JsUser.greetings = function(){
+console.log(`'Welcome back, ${this.name}`);
+}
+console.log(JsUser.greetings());
+
+
 console.log(typeof mySym);
 console.log(JsUser);
 
 JsUser.email = "mananjoshi@chatgpt.com";
 console.log(JsUser);
-Object.freeze(JsUser);
+// Object.freeze(JsUser);
 JsUser.email = "mananjoshi@microsoft.com";
+
+JsUser.greetings = function(){
+    console.log("Hello Js User");
+}
+JsUser.greetingsTwo = function(){
+    console.log(`Hello JsUSer ${this.name}`);
+}
+console.log(JsUser.greetings());
+console.log(JsUser.greetingsTwo());
