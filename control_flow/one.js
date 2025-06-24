696058a4812ace643021e7a22108b6564346d738
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-24 03:37:46 +0000

    one

--- control_flow/one.js
+++ control_flow/one.js
@@ -20,8 +20,17 @@ let val1;
 // val1 = null ?? 10   // 10
 // val1 = undefined ?? // 15
 // val1 = null ?? 10 ?? 20 // 10
-
 console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more then 80")
+
+
 
 const number = 9;
 if(number > 10){
