4b85378b1693a0f317f8c31a6d0ff2c207650da1
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-25 04:41:15 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+let index = 0;
+while (index <= 10) {
+    console.log(`Value of the index is: ${index}`);
+    index ++
+}
+
+
+let myArray = ["flash", "superman", "batman"];
+let i = 0;
+while (i < myArray.length) {
+    console.log(`value is ${myArray[i]}`);
+    i++;
+}
