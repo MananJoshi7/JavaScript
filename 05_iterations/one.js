b2bf302ab6cfaaeb9ef67cab52c52aee6e638dc1
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-25 06:49:49 +0000

    one

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -73,7 +73,7 @@ for (let index = 0; index <= 10; index++) {
 for (let index = 1; index < 9; index++) {
     if(index%2!==0){
         // console.log(index);
-    }  
+    }   
 }
 
 
@@ -162,14 +162,7 @@ for(let i = 1; i <= 3; i++){
 //   console.log(pattern);
 // }
 
-// for (let i = 0; i < 10; i++) {
-//     console.log(`Outer loop value: ${i}`);
-//     for (let j = 0; j < 10; j++) {
-//         // console.log(`Inner loop value: ${j} and inner loop ${i}`)
-//         console.log(i + '*' + j + '=' + i*j)
-//     }
-    
-// }
+
 
 
 
