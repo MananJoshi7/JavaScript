020d8fb1c232ea916a1e3a8cd925a2392f55d05f
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-25 07:04:15 +0000

    two

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -11,3 +11,37 @@ while (i < myArray.length) {
     console.log(`value is ${myArray[i]}`);
     i++;
 }
+
+
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score ++
+} while (score <= 10);
+
+
+// Create a countdown from 10 to 1 using a while loop
+let count = 10;
+while (count >= 1) {
+    // console.log(count);
+    count -- 
+}
+
+
+// Use a while loop to calculate the sum of numbers from 1 to 10.
+let sum = 0;
+let num = 1;
+while (num <= 10) {
+    sum+=num;
+    num++;
+}
+// console.log(`Sum is: ${sum}`)
+
+
+// Write a do...while loop that prints numbers from 1 to 5.
+let num1 = 1;
+do {
+    // console.log(num1);
+    num1 ++;
+    
+} while (num1 <= 5);
