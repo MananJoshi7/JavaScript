a266b04bbb918fc3f9ed73c70f62188dc9de0974
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-06-26 04:42:19 +0000

    seven

---
+++ 05_iterations/seven.js
@@ -0,0 +1,26 @@
+// map
+
+
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+const newNum = numbers.map( (num) => num + 1 );
+console.log(newNum);
+
+
+
+// const  newNum = numbers.map( (num) =>{
+//     return num + 10
+// } )
+// console.log(newNum);                        // Arrow function
+
+
+// const newNum = numbers.map( function(num){
+//     return num + 2;
+// } )
+// console.log(newNum);                        // Normal function
+
+
+// for each loop
+// const numbers1 = [1,2,3,4,5,6,7,8,9,10];
+// numbers.forEach( (num) => {
+//     console.log(num+5);
+// } )
