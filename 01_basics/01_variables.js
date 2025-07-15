8bcd064f8b35b398cda3b7a0cb6e279c58ea76bf
MananJoshi16 <121673401+MananJoshi7@users.noreply.github.com>
2025-07-15 05:30:36 +0000

    var

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,34 @@
 // var window mein add hota hai
 // var function scoped hota hai
 // var aap firse same name sai use kar sakto ho error nahi aaye ga
+// var a = 9;         // global scope
+
+// {
+//     let a = 9;     // blocked scope
+// }
+
+// function abcd(){
+//     if(true){
+//         var a = 9; // functional scope
+//     }
+// }
+
+// // Reassigment, Redeclaration 
+// // Redecalaration is only possible with var
+
+// var a = 9;
+// a = 10;
+// var a = 11;
+
+// let b = 9;
+// b = 10;
+// let b = 11; // We can't redeclare the same variable under the let
+
+// Hoisting -> ek varibale ko jab js mai bane te hai to wo varible do hisso mai toot jata hai and uska declare part upper chala jata hai and uska 
+// initialization part neeche reh jata hai
+// console.log(a)
+// let a = 9;
+
 
 
 const accountId = 789
